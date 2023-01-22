680415c0e7490893b282d122759691fa6c98f77a
danazou <dana.sou@gmail.com>
2023-01-21 22:19:09 -0800

    added controller and helper methods with successful postman test

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+const bands = require('./controllers/bands_controller')
+
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+  
+  
 
 // ROOT
 app.get('/', (req, res) => {
@@ -14,7 +22,13 @@ app.get('/', (req, res) => {
     })
 })
 
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
+
+
