e30c166f7faad4a55607543ccb49a3164b0a4c86
danazou <dana.sou@gmail.com>
2023-01-24 14:26:24 -0800

    added band model association

--- models/band.js
+++ models/band.js
@@ -2,13 +2,17 @@
 const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Band extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({ Meet_Greet, Set_Time }) {
+      //   meet and greets
+      Band.hasMany(Meet_Greet, {
+        foreignKey: 'band_id',
+        as: 'meet_greets',
+      })
+      //   set time
+      Band.hasMany(Set_Time, {
+        foreignKey: 'band_id',
+        as: 'set_times',
+      })
     }
   }
   Band.init({
